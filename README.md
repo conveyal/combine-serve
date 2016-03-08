900ea32e99b4589d3cbb9de4e4913b226bc1f19b
Matthew Wigginton Conway <matt@indicatrix.org>
2016-03-08 15:31:30 -0500

    initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# combine-serve: combine static files and API endpoints and serve them as a unified server
+
+Used to combine frontend and backend processes in development. For instance, if you're using node to develop your frontend and running a Java backend on port 7070:
+
+	combine-serve --serve /client build/ --proxy / http://localhost:7070 --port 7071
+
+will create a server running on port 7071 that serves the files in the build directory at http://localhost:7071/client and proxies other requests to http://localhost:7070/
---
+++ index.js
@@ -0,0 +1,33 @@
+/** Combine multiple APIs together and serve them as one */
+
+import connect from 'connect'
+import proxy from 'proxy-middleware'
+import serveStatic from 'serve-static'
+
+// parse the arguments
+let args = process.argv.slice(2)
+
+let app = connect()
+
+let port = 7941
+
+for (let i = 0; i < args.length; i += 3) {
+  let mode = args[i]
+  let path = args[i + 1]
+  let target = args[i + 2]
+
+  if (!path.startsWith('/')) path = '/' + path
+
+  if (mode === '--proxy') {
+    app.use(path, proxy(target))
+  } else if (mode === '--serve') {
+    app.use(path, serveStatic(target))
+  } else if (mode === '--port') {
+    port = Number(args[i + 1])
+    i-- // only one option
+  }
+}
+
+app.listen(port)
+
+console.log(`Listening for HTTP on port ${port}`)
